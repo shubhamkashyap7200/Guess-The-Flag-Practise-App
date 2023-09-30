8f04b40b352b8ca1a0a2780e4667d3c9839845b1
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-09-30 17:28:04 +0530

    Testing

--- Guess The Flag Practise App/ContentView.swift
+++ Guess The Flag Practise App/ContentView.swift
@@ -9,16 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.orange
+                .ignoresSafeArea()
+
+            HStack {
+                VerticalBoxView()
+                VerticalBoxView()
+                VerticalBoxView()
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct VerticalBoxView: View {
+    var body: some View {
+        VStack() {
+            TextBoxView()
+            TextBoxView()
+            TextBoxView()
+            
+        }
+    }
+}
+
+struct TextBoxView: View {
+    var body: some View {
+        Text("O")
+            .frame(width: 70, height: 70)
+            .foregroundStyle(Color.white)
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10.0))
+    }
 }
