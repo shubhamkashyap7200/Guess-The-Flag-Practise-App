113dd4f214d6c439e42d9475b1758346518c229b
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-09-30 18:46:59 +0530

    Commit 2

--- Guess The Flag Practise App/ContentView.swift
+++ Guess The Flag Practise App/ContentView.swift
@@ -8,15 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Spain", "UK", "US", "Russia", "Nigeria", "Italy"].shuffled()
+    
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
         ZStack {
-            Color.orange
+            Color.blue
                 .ignoresSafeArea()
-
-            HStack {
-                VerticalBoxView()
-                VerticalBoxView()
-                VerticalBoxView()
+            
+            VStack(spacing: 30.0) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundStyle(Color.white)
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(Color.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button{
+                        
+                    }
+                label: {
+                    Image(countries[number])
+                        .renderingMode(.original)
+                }
+                }
             }
         }
     }
@@ -26,23 +43,69 @@ struct ContentView: View {
     ContentView()
 }
 
-struct VerticalBoxView: View {
-    var body: some View {
-        VStack() {
-            TextBoxView()
-            TextBoxView()
-            TextBoxView()
-            
-        }
-    }
-}
 
-struct TextBoxView: View {
-    var body: some View {
-        Text("O")
-            .frame(width: 70, height: 70)
-            .foregroundStyle(Color.white)
-            .background(Color.blue)
-            .clipShape(RoundedRectangle(cornerRadius: 10.0))
-    }
-}
+
+
+
+//        LinearGradient(
+//            stops: [
+//                Gradient.Stop(color: .green, location: 0.25), Gradient.Stop(color: .yellow, location: 0.75)
+//            ],
+//            startPoint: .topLeading, endPoint: .bottomTrailing)
+
+//        RadialGradient(gradient: Gradient(colors: [.indigo,.black]), center: .center, startRadius: 20, endRadius: 180)
+
+//        AngularGradient(colors: [Color.green, Color.red], center: UnitPoint.center)
+
+//        VStack(spacing: 20.0) {
+//
+//            Button("Button 1") { }
+//                .buttonStyle(.bordered)
+//
+//            Button("Button 2", role: .destructive) { }
+//                .buttonStyle(.bordered)
+//
+//            Button("Button 3") { }
+//                .buttonStyle(.borderedProminent)
+//
+//            Button("Button 3") { }
+//                .buttonStyle(.borderedProminent)
+//                .tint(.purple)
+//
+//            Button("Button 4", role: .destructive) { }
+//                .buttonStyle(.borderedProminent)
+//
+//            Button {
+//
+//            } label: {
+//                Text("Custom Button Pressed")
+//                    .padding()
+//                    .font(.system(size: 25.0))
+//                    .foregroundStyle(Color.white)
+//                    .background(Color.green)
+//                    .clipShape(RoundedRectangle(cornerRadius: 10.0))
+//            }
+//
+//
+//            Button {
+//
+//            } label: {
+//                Label("Edit", systemImage: "pencil")
+//                    .font(.system(.title))
+//            }
+//
+//
+//        }
+//
+//
+//// MARK: Alerts practise
+//Button("Show Alert") {
+//    showingAlert = true
+//}
+//.alert("Message here", isPresented: $showingAlert) {
+//    Button("Delete", role: .destructive) { }
+//    Button("Cancel", role: .cancel) { }
+//} message: {
+//    Text("Read this twice")
+//}
+//
