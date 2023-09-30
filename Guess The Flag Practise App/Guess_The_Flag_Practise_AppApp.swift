6d488366bdcb36a8acc5374d0fe2c24f14fb9b55
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-09-30 17:09:37 +0530

    Initial Commit

---
+++ Guess The Flag Practise App/Guess_The_Flag_Practise_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_Flag_Practise_AppApp.swift
+//  Guess The Flag Practise App
+//
+//  Created by Shubham on 30/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_Flag_Practise_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
